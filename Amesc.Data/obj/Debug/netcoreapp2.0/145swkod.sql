798f91d3d9ac1a948331ea3080959cb852e362c5
Stephany Henrique A Batista <salmeidabatista@gmail.com>
2017-08-23 14:05:40 -0400

    deletando pastas desnecessárias e colocando no ignore

--- Amesc.Data/obj/Debug/netcoreapp2.0/145swkod.sql
+++
@@ -1,9 +0,0 @@
-﻿ALTER TABLE [Alunos] ADD [Endereco_Cep] nvarchar(max) NULL;
-
-GO
-
-INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
-VALUES (N'20170823173628_AddCepEmEndereco', N'2.0.0-rtm-26452');
-
-GO
-
