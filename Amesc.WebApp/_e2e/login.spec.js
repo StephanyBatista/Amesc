60bb0488b0878bca8b8d5ae92e8edeb5e6636d75
Stephany Henrique <stephany.batista@outlook.com>
2018-01-20 20:45:32 -0300

    instalando o protactor

---
+++ Amesc.WebApp/_e2e/login.spec.js
@@ -0,0 +1,10 @@
+﻿// spec.js
+//http://www.protractortest.org/#/tutorial
+//https://medium.com/@marcelmokos/end-to-end-testing-with-protractor-using-modern-javascript-syntax-44e5121c2e03
+describe('Protractor Demo App', function () {
+    it('should have a title', function () {
+        browser.get('http://juliemr.github.io/protractor-demo/');
+
+        expect(browser.getTitle()).toEqual('Super Calculator');
+    });
+});
