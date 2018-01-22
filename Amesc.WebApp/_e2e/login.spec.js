90c59d1cd3bcb9a12dabab293a354b914919abbd
Stephany Henrique A Batista <stephany.batista@db1.com.br>
2018-01-22 13:35:43 -0300

    Configurando o login com protractor

--- Amesc.WebApp/_e2e/login.spec.js
+++ Amesc.WebApp/_e2e/login.spec.js
@@ -3,7 +3,18 @@
 //https://medium.com/@marcelmokos/end-to-end-testing-with-protractor-using-modern-javascript-syntax-44e5121c2e03
 describe('Protractor Demo App', function () {
     it('should have a title', function () {
-        browser.get('http://juliemr.github.io/protractor-demo/');
+
+        browser.ignoreSynchronization = true;
+
+        browser.get('http://localhost:50643/Autenticacao');
+
+        const email = 'fulano@fulano.com';
+        const password = '123456';
+
+        element(by.css('[name="Email"]')).sendKeys(email);
+        element(by.css('[name="Password"]')).sendKeys(password);
+
+        element(by.css('.btn-default')).click();
 
         expect(browser.getTitle()).toEqual('Super Calculator');
     });
